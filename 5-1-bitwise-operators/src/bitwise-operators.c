3dd1276dd12973fa54986861cf1d86013c21c55f
yannickkirschen <yannickkirschen@protonmail.com>
2021-10-16 17:43:36 +0200

    feat: add assignment for bitwise operations

---
+++ 5-1-bitwise-operators/src/bitwise-operators.c
@@ -0,0 +1,50 @@
+// TO DO 1: Something is wrong with the output
+//	        Hint: put the output first into an additional variable m,
+//	        use bitwise operators and count the digits
+// TO DO 2: A second number should be entered and
+//	        both numbers should be combined with the various bitwise
+//	        operators before providing the output
+// TO DO 3: Use signed integers and see how negative numbers
+//          are presented in binary format
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printBinary(int i) {
+    printf("Binary representation of %i: ", i);
+
+    for (int c = 31; c >= 0; c--) {
+        printf("%d", i >> c & 1);
+    }
+
+    printf("\n");
+}
+
+void printUnsignedBinary(unsigned int i) {
+    printf("Binary representation of %u: ", i);
+
+    for (int c = 31; c >= 0; c--) {
+        printf("%d", i >> c & 1);
+    }
+
+    printf("\n");
+}
+
+int main() {
+    unsigned int n1;
+    unsigned int n2;
+    int n3;
+
+    printf("Enter an integer number: ");
+    scanf("%u", &n1);
+
+    printf("Enter another integer number: ");
+    scanf("%u", &n2);
+
+    printf("Enter a negative integer number: ");
+    scanf("%i", &n3);
+
+    printUnsignedBinary(n1);
+    printUnsignedBinary(n2);
+    printBinary(n3);
+}
