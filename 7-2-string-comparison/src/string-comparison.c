40f6ad65df18d3025669b2f6c0876917c29f8316
yannickkirschen <yannickkirschen@protonmail.com>
2021-10-18 17:28:29 +0200

    feat: add string comparison
    Signed-off-by: yannickkirschen <yannickkirschen@protonmail.com>

---
+++ 7-2-string-comparison/src/string-comparison.c
@@ -0,0 +1,114 @@
+/* Compare two strings */
+
+// TO DO 1: This program does not always give the correct results. What is the problem? Correct it!
+// TO DO 2: Adapt the comparison such that it can be used for providing an ordering e.g. for phone books
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * This will check two strings on equality based on their characters.
+ *
+ * @param s1 First string.
+ * @param s2 Second string.
+ * @return true, if both strings are equal. Otherwise false.
+ */
+bool equals(const char *s1, const char *s2) {
+    int i = 0;
+
+    char c1;
+    char c2;
+
+    do {
+        c1 = s1[i];
+        c2 = s2[i];
+
+        if (c1 != c2) {
+            return false;
+        }
+
+        i++;
+    } while (c1 != '\0' && c2 != '\0');
+
+    if (c1 == '\0' && c2 == '\0') {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * Compares two strings lexicographical.
+ *
+ * 0: equal (s1 = s2)
+ * 1: ordered as input (s1, s2)
+ * -1: not ordered as input (s2, s1)
+ *
+ * @param s1 First string.
+ * @param s2 Second string.
+ * @return 0, if both strings are equal. 1, if the order is s1 -> s2.
+ * -1, if the order is s2 -> s1.
+ */
+int compare(const char *s1, const char *s2) {
+    int i = 0;
+
+    char c1;
+    char c2;
+
+    do {
+        c1 = s1[i];
+        c2 = s2[i];
+
+        if (c1 < c2) {
+            return 1;
+        } else if (c1 > c2) {
+            return -1;
+        }
+
+        i++;
+    } while (c1 != '\0' && c2 != '\0');
+
+    // Both strings are the same length.
+    // As we made it here, we know they're equal.
+    if (c1 == '\0' && c2 == '\0') {
+        return 0;
+    }
+
+    // When the first string reached its end and the second doesn't, we know the
+    // second is longer and as we masde it here, it has to be ordered after the
+    // first one.
+    if (c1 == '\0' && c2 != '\0') {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    char s1[100];
+    char s2[100];
+
+    printf("Please enter string 1: ");
+    scanf("%[^\n]", s1);
+
+    getchar();
+
+    printf("Please enter string 2: ");
+    scanf("%[^\n]", s2);
+
+    if (equals(s1, s2)) {
+        printf("Strings are equal.\n");
+    } else {
+        printf("Strings are different!\n");
+    }
+
+    int comparison = compare(s1, s2);
+    if (comparison == 0) {
+        printf("Strings are equal.\n");
+    } else if (comparison > 0) {
+        printf("s1, s2: %s, %s\n", s1, s2);
+    } else {
+        printf("s2, s1: %s, %s\n", s2, s1);
+    }
+}
